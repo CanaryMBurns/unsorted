442416b43aaaa5be644e142f0760da25b0f3b794
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2020-09-25 21:54:21 +0200

    Project and basic config ok.

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+import BarChart from './components/BarChart.svelte'
+
+// Configuration
+let set = [1, 2, 3];
+</script>
+
+<main class="main">
+	<BarChart {set}/>
+</main>
+
+<style>
+.main {
+	width: 100%;
+	height: 100%;
+}
+</style>
---
+++ src/components/BarChart.svelte
@@ -0,0 +1,23 @@
+<script>
+export let set = [];
+</script>
+
+<div class="bar-chart">
+  {#each set as element}
+    <div class="bar"></div>
+	{/each}
+</div>
+
+<style>
+.bar-chart {
+  display: flex;
+	width: 100%;
+	height: 100%;
+}
+
+.bar {
+  width: 150px;
+  height: 100%;
+  background-color: tomato;
+}
+</style>
